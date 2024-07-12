8f612a30472830a7704b07d86bdcae721abd9bde
guilleferrioldev <guillermoferrioldev@gmail.com>
2024-07-12 11:25:37 -0400

    created the function to apply the lemmatization

--- server/router.py
+++ server/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from fastapi.responses import ORJSONResponse
-from utils import split_this, remove_stopwords, load_json
+from utils import apply_from_split_to_lemmatization, load_json
 
 router = APIRouter()
 
@@ -9,6 +9,6 @@ async def get_prediction(symptoms: str) -> ORJSONResponse:
     try:
         plants = load_json("plantas.json")["plantas"]
 
-        return ORJSONResponse({"status": "success", "input": remove_stopwords(split_this(symptoms))}, status_code=200)
+        return ORJSONResponse({"status": "success", "input": apply_from_split_to_lemmatization(symptoms)}, status_code=200)
     except Exception as err:
         return ORJSONResponse({"status": "error", "message": err.args[0]}, status_code=400)
--- server/utils.py
+++ server/utils.py
@@ -3,12 +3,21 @@ from re import findall
 from nltk.corpus import stopwords
 import json
 
-def split_this(sentence: str) -> List[str]:
+import spacy
+
+def split_sentence(sentence: str) -> List[str]:
     return findall(r'\b\w+\b', sentence.lower())
 
 def remove_stopwords(list_to_remove: List[str]) -> List[str]:
     stops = set(stopwords.words('spanish'))
     return [word for word in list_to_remove if word not in stops]
+
+def lemmatization(words: List[str]) -> List[str]:
+    nlp = spacy.load("es_core_news_sm")
+    return [token.lemma_ for word in words for token in nlp(word)]
+
+def apply_from_split_to_lemmatization(sentence: str) -> List[str]:
+    return lemmatization(remove_stopwords(split_sentence(sentence)))
 
 def load_json(path: str):
     with open(path, 'r') as file:
