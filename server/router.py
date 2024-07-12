2ab1dc494452139778a982395fb3c6084f5eb50a
guilleferrioldev <guillermoferrioldev@gmail.com>
2024-07-12 10:33:58 -0400

    added requirements.txt

--- server/router.py
+++ server/router.py
@@ -6,4 +6,9 @@ router = APIRouter()
 
 @router.get("/plants")
 async def get_prediction(symptoms: str) -> ORJSONResponse:
-    return ORJSONResponse({"status": "success", "input": remove_stopwords(split_this(symptoms)), "plantas": load_json("plantas.json")}, status_code=200)
+    try:
+        plants = load_json("plantas.json")["plantas"]
+
+        return ORJSONResponse({"status": "success", "input": remove_stopwords(split_this(symptoms))}, status_code=200)
+    except Exception as err:
+        return ORJSONResponse({"status": "error", "message": err.args[0]}, status_code=400)
